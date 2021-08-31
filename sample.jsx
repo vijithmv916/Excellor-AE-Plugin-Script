1fb1b59bc57f63fe7b7ee433af76058f992d42f6
vijithmv916 <miniboyviju916@gmail.com>
2021-08-31 15:44:42 +0530

    uploaded nut hav bug inpoint

--- sample.jsx
+++ sample.jsx
@@ -1,22 +1,45 @@
 
 
+{
+    function lineEaser() {
 
-var pathLine = app.project.activeItem.selectedLayers[0]
-try{
-    var trimPath = pathLine.property("Contents").addProperty("ADBE Vector Filter - Trim")
-}catch(e) {
-    alert("No Layer Selected");
+        var pathLine = app.project.activeItem.selectedLayers[0]
+        try {
+            var trimPath = pathLine.property("Contents").addProperty("ADBE Vector Filter - Trim")
+        } catch (e) {
+            alert("No Layer Selected");
+        }
+        controlEnd = trimPath.property("ADBE Vector Trim End")
+
+        var easeIn = new KeyframeEase(.5, 70);
+        var easeOut = new KeyframeEase(.5, 70);
+
+        // key at second
+        controlEnd.addKey(0)
+        controlEnd.addKey(0.75)
+        controlEnd.setValueAtKey(1, 0)
+        controlEnd.setValueAtKey(2, 100)
+
+        controlEnd.setTemporalEaseAtKey(1, [easeIn], [easeOut]);
+        controlEnd.setTemporalEaseAtKey(2, [easeIn], [easeOut]);
     }
-controlEnd = trimPath.property("ADBE Vector Trim End")
 
-var easeIn = new KeyframeEase(.5, 70);
-var easeOut = new KeyframeEase(.5, 70);
+    function createUI(thisObj) {
+        var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "My Tools",
+            undefined);
+        var btn = myPanel.add("button", [10, 10, 90, 40], "Draw");
+        try {
+            btn.onClick = function () { lineEaser() }
+        } catch (e) { }
 
-// key at second
-controlEnd.addKey(0)
-controlEnd.addKey(0.75)
-controlEnd.setValueAtKey(1, 0)
-controlEnd.setValueAtKey(2, 100)
+        return myPanel;
+    }
+    var myToolsPanel = createUI(this);
 
-controlEnd.setTemporalEaseAtKey(1, [easeIn], [easeOut]);
-controlEnd.setTemporalEaseAtKey(2, [easeIn], [easeOut]);
+    // myToolsPanel.show()
+
+
+
+
+    // Excellor_Flash(this)
+}
